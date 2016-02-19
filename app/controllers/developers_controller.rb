81c8222bdc3aa3da86511dc2ca21389aaead042c
Vidal Ekechukwu <vekechukwu@gmail.com>
2016-02-19 16:53:48 +0000

    Developer with most recent commit at top of list.

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -9,14 +9,23 @@
 class DevelopersController < ApplicationController
   def index
     developers = Developer.find_by_sql(%Q[
-      select d.*, daj.array_json as activities
+      select d.*, da.array_json as activities,
+      da.most_recent_date
       from developers d
+
+      -- Developer Activity
       join (
-        select daj1.developer_id,
-        to_json(array_agg(daj1)) array_json
-        from developer_activities daj1
-        group by daj1.developer_id
-      ) daj on daj.developer_id=d.id
+        select da1.developer_id,
+        to_json(array_agg(da1)) array_json,
+        min(
+          da1.event_occurred_at
+          order by event_occurred_at desc
+        ) as most_recent_date
+        from developer_activities da1
+        group by da1.developer_id
+      ) da on da.developer_id=d.id
+
+      order by da.most_recent_date desc
     ])
 
     respond_to do |format|
--- spec/features/developer_sees_commit_spec.rb
+++ spec/features/developer_sees_commit_spec.rb
@@ -89,4 +89,106 @@ RSpec.feature "Hacker list" do
     expect(activity['repo_name']).to eq 'VEkh/sideprojects'
     expect(activity['commits'].values).to include 'Migrating to Hashrocket git'
   end
+
+  scenario "Developer commits and sees self at the top of the list", type: :request do
+    # https://api.github.com/users/vekh/events
+    vekh_events = [
+      {
+        "id" => "3650080452",
+        "type" => "PushEvent",
+        "actor" => {
+          "id" => 735821,
+          "login" => "VEkh",
+          "gravatar_id" => "",
+          "url" => "https://api.github.com/users/VEkh",
+          "avatar_url" => "https://avatars.githubusercontent.com/u/735821?"
+        },
+        "repo" => {
+          "id" => 51382870,
+          "name" => "VEkh/sideprojects",
+          "url" => "https://api.github.com/repos/VEkh/sideprojects"
+        },
+        "payload" => {
+          "push_id" => 979550639,
+          "size" => 1,
+          "distinct_size" => 1,
+          "ref" => "refs/heads/master",
+          "head" => "30cbf09da0778e4f1eb0bc94575d858d68ea95d6",
+          "before" => "6ae4312e0b28cd32d3b49e11ecae68bbe9b58d62",
+          "commits" => [
+            {
+              "sha" => "30cbf09da0778e4f1eb0bc94575d858d68ea95d6",
+              "author" => {
+                "email" => "vekechukwu@gmail.com",
+                "name" => "Vidal Ekechukwu"
+              },
+              "message" => "Migrating to Hashrocket git",
+              "distinct" => true,
+              "url" => "https://api.github.com/repos/VEkh/sideprojects/commits/30cbf09da0778e4f1eb0bc94575d858d68ea95d6"
+            }
+          ]
+        },
+        "public" => true,
+        "created_at" => "2016-02-16T15:33:52Z"
+      }
+    ]
+
+    vekh = vekh_events.first['actor']
+
+    Developer.create_with_json(vekh)
+    DeveloperActivity.create_with_json(vekh_events)
+
+    # https://api.github.com/users/chriserin/events
+    chriserin_events = [
+      {
+        "id" => "3613641536",
+        "type" => "PushEvent",
+        "actor" => {
+          "id" => 597909,
+          "login" => "chriserin",
+          "gravatar_id" => "",
+          "url" => "https://api.github.com/users/chriserin",
+          "avatar_url" => "https://avatars.githubusercontent.com/u/597909?"
+        },
+        "repo" => {
+          "id" => 31873570,
+          "name" => "chriserin/seq27",
+          "url" => "https://api.github.com/repos/chriserin/seq27"
+        },
+        "payload" => {
+          "push_id" => 966181213,
+          "size" => 1,
+          "distinct_size" => 1,
+          "ref" => "refs/heads/master",
+          "head" => "1007f813c367db8a610ea7a515ee25fb90bc498f",
+          "before" => "57779ce19b72f5c4ea4146e98e0d73efbf9bdcc1",
+          "commits" => [
+            {
+              "sha" => "1007f813c367db8a610ea7a515ee25fb90bc498f",
+              "author" => {
+                "email" => "dev@hashrocket.com",
+                "name" => "Hashrocket Workstation"
+              },
+              "message" => "Upgrade rails -> 4.2.5:W",
+              "distinct" => true,
+              "url" => "https://api.github.com/repos/chriserin/seq27/commits/1007f813c367db8a610ea7a515ee25fb90bc498f"
+            }
+          ]
+        },
+        "public" => true,
+        "created_at" => "2016-02-05T21:18:31Z"
+      }
+    ]
+
+    chriserin = chriserin_events.first['actor']
+
+    Developer.create_with_json(chriserin)
+    DeveloperActivity.create_with_json(chriserin_events)
+
+    get '/developers.json'
+    developers = JSON.parse(response.body)
+
+    # Then the developer sees their card at the top of the list
+    expect(developers.first['name']).to eq 'VEkh'
+  end
 end
