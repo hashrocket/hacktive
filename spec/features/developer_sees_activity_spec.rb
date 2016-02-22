378c9b523d95e89f70d7f2aa69e98885cb0af033
Vidal Ekechukwu <vekechukwu@gmail.com>
2016-02-22 19:42:44 +0000

    Developer sees timestamp of Github action
    [#113664013]

--- spec/features/developer_sees_activity_spec.rb
+++ spec/features/developer_sees_activity_spec.rb
@@ -438,4 +438,94 @@ RSpec.feature "Hacker list" do
     expect(most_recent_activity['repo_name']).to eq 'hashrocket/hr_hotels'
     expect(most_recent_activity['payload']['57790579']).to eq 'opened'
   end
+
+  scenario 'Developer sees time description for a recent commit', type: :request do
+    # https://api.github.com/users/vekh
+    github_developer = {
+      "login" => "VEkh",
+      "id" => 735821,
+      "avatar_url" => "https://avatars.githubusercontent.com/u/735821?v=3",
+      "gravatar_id" => "",
+      "url" => "https://api.github.com/users/VEkh",
+      "html_url" => "https://github.com/VEkh",
+      "followers_url" => "https://api.github.com/users/VEkh/followers",
+      "following_url" => "https://api.github.com/users/VEkh/following{/other_user}",
+      "gists_url" => "https://api.github.com/users/VEkh/gists{/gist_id}",
+      "starred_url" => "https://api.github.com/users/VEkh/starred{/owner}{/repo}",
+      "subscriptions_url" => "https://api.github.com/users/VEkh/subscriptions",
+      "organizations_url" => "https://api.github.com/users/VEkh/orgs",
+      "repos_url" => "https://api.github.com/users/VEkh/repos",
+      "events_url" => "https://api.github.com/users/VEkh/events{/privacy}",
+      "received_events_url" => "https://api.github.com/users/VEkh/received_events",
+      "type" => "User",
+      "site_admin" => false,
+      "name" => "Vidal Ekechukwu",
+      "company" => nil,
+      "blog" => nil,
+      "location" => nil,
+      "email" => nil,
+      "hireable" => nil,
+      "bio" => nil,
+      "public_repos" => 2,
+      "public_gists" => 0,
+      "followers" => 1,
+      "following" => 2,
+      "created_at" => "2011-04-18T04:48:52Z",
+      "updated_at" => "2016-02-16T17:23:23Z"
+    }
+
+    # https://api.github.com/users/vekh/events
+    github_developer_events = [
+      {
+        "id" => "3650080452",
+        "type" => "PushEvent",
+        "actor" => {
+          "id" => 735821,
+          "login" => "VEkh",
+          "gravatar_id" => "",
+          "url" => "https://api.github.com/users/VEkh",
+          "avatar_url" => "https://avatars.githubusercontent.com/u/735821?"
+        },
+        "repo" => {
+          "id" => 51382870,
+          "name" => "VEkh/sideprojects",
+          "url" => "https://api.github.com/repos/VEkh/sideprojects"
+        },
+        "payload" => {
+          "push_id" => 979550639,
+          "size" => 1,
+          "distinct_size" => 1,
+          "ref" => "refs/heads/master",
+          "head" => "30cbf09da0778e4f1eb0bc94575d858d68ea95d6",
+          "before" => "6ae4312e0b28cd32d3b49e11ecae68bbe9b58d62",
+          "commits" => [
+            {
+              "sha" => "30cbf09da0778e4f1eb0bc94575d858d68ea95d6",
+              "author" => {
+                "email" => "vekechukwu@gmail.com",
+                "name" => "Vidal Ekechukwu"
+              },
+              "message" => "Migrating to Hashrocket git",
+              "distinct" => true,
+              "url" => "https://api.github.com/repos/VEkh/sideprojects/commits/30cbf09da0778e4f1eb0bc94575d858d68ea95d6"
+            }
+          ]
+        },
+        "public" => true,
+        "created_at" => "2016-02-16T15:33:52Z"
+      }
+    ]
+
+    Developer.create_with_json(github_developer)
+    DeveloperActivity.create_with_json(github_developer_events)
+
+    get '/developers.json'
+    top_developer = JSON.parse(response.body).first
+    most_recent_activity = top_developer['activities'].first
+
+    expect(top_developer['name']).to eq 'VEkh'
+    expect(
+      Time.parse(most_recent_activity['event_occurred_at'])
+    ).to eq Time.parse('2016-02-16T15:33:52Z')
+  end
 end
