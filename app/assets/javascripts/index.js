bbb90a3b5397c84c5d61bb6832e319f352bf68b7
Vidal Ekechukwu <vekechukwu@gmail.com>
2016-03-08 20:56:49 +0000

    Recovering ui from 'sideprojects'

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,13 @@
+// Dependencies:
+// * jquery->jquery_ujs+
+// * react->react_ujs+
+
+//= require jquery
+//= require react
+
+//= require jquery_ujs
+//= require moment.min
+//= require react_ujs
+//= require turbolinks
+
+//= require application
