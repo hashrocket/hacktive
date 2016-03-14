f4ee3e10af116390a5585b6ad2defa357548831b
Chris Erin and Vidal Ekechukwu <dev+chriserin+vekh@hashrocket.com>
2016-03-14 09:46:33 -0500

    Starting to implement Flux

---
+++ app/frontend/javascripts/dispatcher/hacktive_dispatcher.js
@@ -0,0 +1,18 @@
+var Dispatcher = require('./Dispatcher');
+var assign = require('object-assign');
+
+var HacktiveDispatcher = assign({}, Dispatcher.prototype, {
+  /**
+   * A bridge function between the views and the dispatcher, marking the action
+   * as a view action.  Another variant here could be handleServerAction.
+   * @param  {object} action The data coming from the view.
+   */
+  handleViewAction: function(action) {
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    });
+  }
+});
+
+module.exports = HacktiveDispatcher;
