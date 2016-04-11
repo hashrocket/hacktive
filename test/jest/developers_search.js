853255e583d61ffa4c771cf9d5711622ca4e1f4c
Hashrocket Workstation <dev@hashrocket.com>
2016-04-11 16:55:21 -0500

    JS testing directory aliases are configured

--- test/jest/developers_search.js
+++ test/jest/developers_search.js
@@ -1,3 +1,18 @@
+var searchModulePath = `${js_root}/components/search`;
+
+jest.unmock(searchModulePath)
+
+var React = require("react");
+var ReactDOM = require("react-dom");
+var TestUtils = require("react-addons-test-utils");
+
+var Search = require(searchModulePath);
+
 describe("DeveloperList", function(){
-  expect("foo").toEqual("foo")
+  it("updates with search", function(){
+    var search = TestUtils.renderIntoDocument(<Search/>);
+    var searchNode = ReactDOM.findDOMNode(search);
+
+    expect(searchNode.innerHTML).toContain("Search")
+  })
 })
