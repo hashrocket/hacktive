f7573eb9a7ba222506eafa3dc26647c44cf5de5b
Dorian Karter and Vidal Ekechukwu <dev+dkarter+vekh@hashrocket.com>
2016-04-29 11:43:30 -0500

    Slightly changed how ajax requests are tested

--- test/javascript/components/developer_list_spec.js
+++ test/javascript/components/developer_list_spec.js
@@ -1,16 +1,15 @@
 import { expect } from "chai";
+import $ from "jquery";
 import React from "react";
-import TestUtils from "react-addons-test-utils";
+import ReactTestUtils from "react-addons-test-utils";
 
 import DeveloperList from "components/developer_list";
 
 describe("Developer List", function(){
-  it("requests developers on mount", function () {
-    const $ = require("jquery");
+  it("requests developers on mount", sinon.test(function(){
+    this.spy($, "ajax")
 
-    sinon.spy($, "ajax")
-
-    const developerList = TestUtils.renderIntoDocument(<DeveloperList/>);
+    const developerList = ReactTestUtils.renderIntoDocument(<DeveloperList/>);
     const ajaxArgs = $.ajax.args[0][0];
 
     expect($.ajax.calledOnce).to.be.true;
