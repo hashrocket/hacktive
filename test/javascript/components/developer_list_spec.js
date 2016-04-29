97026a03db5c917b8cbeaef39c53be2cfed1419a
Dorian Karter and Vidal Ekechukwu <dev+dkarter+vekh@hashrocket.com>
2016-04-29 11:48:33 -0500

    Test: Developer lists fetched developers

--- test/javascript/components/developer_list_spec.js
+++ test/javascript/components/developer_list_spec.js
@@ -1,9 +1,12 @@
 import { expect } from "chai";
 import $ from "jquery";
 import React from "react";
+import ReactDOM from "react-dom";
 import ReactTestUtils from "react-addons-test-utils";
 
+import DeveloperCard from "components/developer_card";
 import DeveloperList from "components/developer_list";
+import DeveloperStore from "flux/stores/developer_store";
 
 describe("Developer List", function(){
   it("requests developers on mount", sinon.test(function(){
@@ -15,5 +18,20 @@ describe("Developer List", function(){
     expect($.ajax.calledOnce).to.be.true;
     expect(ajaxArgs.type).to.equal("GET");
     expect(ajaxArgs.url).to.equal("/developers");
+  }));
+
+  it("shows fetched developers", function(){
+    const developer = chai.create("developer")
+
+    DeveloperStore.setDevelopers([developer])
+
+    const developerList = ReactTestUtils.renderIntoDocument(<DeveloperList/>);
+    const developerListNode = ReactDOM.findDOMNode(developerList);
+    const developerCards = ReactTestUtils.scryRenderedComponentsWithType(
+      developerList,
+      DeveloperCard
+    );
+
+    expect(developerCards.length).to.equal(1)
   });
 });
