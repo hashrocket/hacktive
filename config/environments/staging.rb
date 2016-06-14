8c9023f1c4936d90742b9f0c21aff3bac1a87165
Vidal Ekechukwu <dev+vekh@hashrocket.com>
2016-06-14 16:20:30 -0500

    Separating staging and production environments

---
+++ config/environments/staging.rb
@@ -0,0 +1,15 @@
+Rails.application.configure do
+  config.action_controller.perform_caching = true
+  config.active_record.dump_schema_after_migration = false
+  config.active_support.deprecation = :notify
+  config.assets.digest = true
+  config.cache_classes = true
+  config.consider_all_requests_local       = false
+  config.eager_load = true
+  config.i18n.fallbacks = true
+  config.log_formatter = ::Logger::Formatter.new
+  config.log_level = :debug
+  config.react.variant = :production
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.webpack[:use_manifest] = true
+end
