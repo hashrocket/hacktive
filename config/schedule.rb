7f2abc716c8a37a02f85131bf06fb8e463451617
Vidal Ekechukwu <dev+vekh@hashrocket.com>
2016-09-14 10:49:32 -0500

    Remote letsencrypt renews correctly

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,9 +4,9 @@ set :output, "log/cron_log.log"
 every 1.day, at: "5:00 am" do
   # Auto-renew TLS certification
   letsencrypt_bin = `which letsencrypt`
+
   if letsencrypt_bin != ""
-    # Renew TLS certification if possible
-    command("#{letsencrypt_bin} renew >> /var/log/le-renew.log")
+    command("#{letsencrypt_bin} renew")
   end
 
   # Restart all background processes
