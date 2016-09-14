f92f646a82c0cfb26db8f725373dfb3331f0b51b
Vidal Ekechukwu <dev+vekh@hashrocket.com>
2016-09-14 10:24:52 -0500

    Deploy .env

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,15 +6,13 @@ set :whenever_identifier, "#{fetch(:application)}_#{fetch(:stage)}"
 after "deploy:updating", "deploy:upload_secrets"
 
 namespace :deploy do
-	desc "Upload sensiitve files to remote server"
-	task :upload_secrets do |t|
-		on roles(:all) do |host|
-			secrets_dir = File.join("config", "secrets")
-			local_env_path = File.join(secrets_dir, "#{fetch(:stage)}", ".env")
-			local_monitrc_path = File.join(secrets_dir, "#{fetch(:stage)}", "monitrc")
+  desc "Upload sensitive files to remote server"
+  task :upload_secrets do |t|
+    on roles(:all) do |host|
+      secrets_dir = File.join("config", "secrets")
+      local_env_path = File.join(secrets_dir, "#{fetch(:stage)}", ".env")
 
-			upload!(local_env_path, release_path)
-			upload!(local_monitrc_path, release_path)
-		end
-	end
+      upload!(local_env_path, release_path)
+    end
+  end
 end
